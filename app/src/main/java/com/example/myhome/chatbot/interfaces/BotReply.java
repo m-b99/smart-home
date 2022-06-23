d8b1064983763317463dea9739865f4b8015ade9
mohamed-baiba <mohamed_baiba@um5.ac.ma>
2022-06-23 03:49:14 +0100

    edit

--- app/src/main/java/com/example/myhome/chatbot/interfaces/BotReply.java
+++
@@ -1,7 +0,0 @@
-package com.example.myhome.chatbot.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-    void callback(DetectIntentResponse returnResponse);
-}
